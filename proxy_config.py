c76468f130e6378e90ffb1f68ae409379b54bab0
Sourcery AI <>
2023-05-18 05:21:26 +0000

    'Refactored by Sourcery'

--- proxy_config.py
+++ proxy_config.py
@@ -1,17 +1,17 @@
 import os
 
-# This can also be replaced with another IP address.
-USE_SSL = True
-REMOTE_HOST = "localhost"
-REMOTE_PORT = 443
-
-if os.getenv('MITM_REMOTE_HOST') != None:
+if os.getenv('MITM_REMOTE_HOST') is None:
+    REMOTE_HOST = "localhost"
+else:
     REMOTE_HOST = os.getenv('MITM_REMOTE_HOST')
-if os.getenv('MITM_REMOTE_PORT') != None:
+if os.getenv('MITM_REMOTE_PORT') is None:
+    REMOTE_PORT = 443
+else:
     REMOTE_PORT = int(os.getenv('MITM_REMOTE_PORT'))
-if os.getenv('MITM_USE_SSL') != None:
+if os.getenv('MITM_USE_SSL') is None:
+    USE_SSL = True
+else:
     USE_SSL = bool(os.getenv('MITM_USE_SSL'))
-
-print('MITM Remote Host: ' + REMOTE_HOST)
-print('MITM Remote Port: ' + str(REMOTE_PORT))
-print('MITM Use SSL ' + str(USE_SSL))
+print(f'MITM Remote Host: {REMOTE_HOST}')
+print(f'MITM Remote Port: {REMOTE_PORT}')
+print(f'MITM Use SSL {USE_SSL}')
